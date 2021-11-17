a623cf513a073c764e2383c8603df8cd1986f2ee
Reason <Reason.grg0@gmail.com>
2021-11-17 21:41:11 +0545

    Base view added

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,6 @@
 from django.urls import include, path
-from rest_framework import routers
-from . import views as heroesViews
-from login import views as loginViews
+from .views import base_view
 
-router = routers.DefaultRouter()
-#router.register(r'heroes', heroesViews.HeroViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", base_view, name="base view"),
 ]
