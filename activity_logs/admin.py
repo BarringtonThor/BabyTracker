8113034ece234d49a940590cbb04122ebf451ed7
Reason <Reason.grg0@gmail.com>
2021-11-30 09:06:15 +0545

    registering models to admin site

--- activity_logs/admin.py
+++ activity_logs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Children, SleepLogs, VaccinationLogs, DiaperChangeLogs, GrowthLogs
 
 # Register your models here.
+admin.site.register(Children)
+admin.site.register(SleepLogs)
+admin.site.register(VaccinationLogs)
+admin.site.register(DiaperChangeLogs)
+admin.site.register(GrowthLogs)
